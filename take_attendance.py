304d4a6dcf82bf276ff2aa0e200c9c45713a1625
Prathyusha-Munagapati <83261716+Prathyusha-Munagapati@users.noreply.github.com>
2021-04-27 08:57:49 +0530

    Reading data using while loop

--- take_attendance.py
+++ take_attendance.py
@@ -13,3 +13,39 @@ record.set(3,640)
 record.set(4,480)
 
 present_data=[]
+while(1):
+    s, img = record.read()
+    for qr in decode(img):
+        data=qr.data.decode('utf-8')
+        #print(data)
+
+        #box around
+        pts = np.array([qr.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        
+        #display on image
+        pts2 = qr.rect
+        
+        if data not in present_data:
+            present_data.append(data)
+            #print(10*'\a')
+            playsound('beep.mp3')
+            color=(0,0,255)
+            cv2.polylines(img,[pts],1,color,5)
+            #cv2.waitKey(3)
+        else:
+            color=(0,255,0)    
+            cv2.polylines(img,[pts],1,color,5)
+            cv2.putText(img,"Thank You! {}".format(data),(pts2[0],pts2[1]),cv2.FONT_HERSHEY_PLAIN, 1,color,2)
+
+    cv2.imshow('Result',img)
+    key = cv2.waitKey(1)
+
+    #to release camera frame
+    if(key==27): #esc key value is 27
+        cv2.destroyWindow("Result")
+        record.release()
+        break
+    
+print(present_data)
+
