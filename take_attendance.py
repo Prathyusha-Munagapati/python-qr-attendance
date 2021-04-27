a72c019a45e390460ce9ea533aa9faf5d7cfb4bb
Prathyusha-Munagapati <83261716+Prathyusha-Munagapati@users.noreply.github.com>
2021-04-27 08:55:23 +0530

    Create take_attendance.py

---
+++ take_attendance.py
@@ -0,0 +1,10 @@
+  
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+from playsound import playsound
+import openpyxl
+import datetime as dt
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
