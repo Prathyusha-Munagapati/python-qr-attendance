67da34e2e32872c8e465d04854ebed4daaa9b927
Prathyusha-Munagapati <83261716+Prathyusha-Munagapati@users.noreply.github.com>
2021-04-27 08:56:08 +0530

    Update take_attendance.py

--- take_attendance.py
+++ take_attendance.py
@@ -8,3 +8,8 @@ import datetime as dt
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+record = cv2.VideoCapture(0)
+record.set(3,640)
+record.set(4,480)
+
+present_data=[]
